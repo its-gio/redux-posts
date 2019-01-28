f66921275c2a69b2a53b20dfcda37c739e7c9c94
its-gio <me@its-g.io>
2019-01-27 23:02:36 -0800

    Adding boiler plate for redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
+import { Provider } from "react-redux";
+
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
+
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Redux Post Practice</h1>
-        <PostForm />
-        <hr />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div>
+          <h1>Redux Post Practice</h1>
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
