71e57a31a762cd79c174fc660d5f8acb9f2d076c
its-gio <me@its-g.io>
2019-01-27 21:55:03 -0800

    Populating posts component with posts from APi and deleting all styles from App.scss

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -12,7 +12,19 @@ class Posts extends Component {
   }
 
   render() {
-    return <div>Posts</div>;
+    const postItems = this.state.posts.map(post => (
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ));
+
+    return (
+      <div>
+        <h1>Posts</h1>
+        {postItems}
+      </div>
+    );
   }
 }
 
