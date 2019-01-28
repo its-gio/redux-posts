9c47e6b909529a38ef26a6ad523659f1aca843d9
its-gio <me@its-g.io>
2019-01-27 21:43:36 -0800

    Added post state array and fetched posts from jsonplaceholder api. Used data to populate posts state.

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 
 class Posts extends Component {
+  state = {
+    posts: []
+  };
+
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then(blob => blob.json())
-      .then(posts => console.log(posts));
+      .then(posts => this.setState({ posts }));
   }
 
   render() {
